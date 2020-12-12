a62422cbaf02245a8db90d44dc8b5df9c8bf5a7f
Ilya <oshlakov2014@gmail.com>
2020-12-12 17:36:30 +0700

    Change a "main" file

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 int main()
 {
     struct RadixTree *node = NULL;
-    node = radixTreeInsert(node, "hello", "345");
+    /*node = radixTreeInsert(node, "admiral", "007");
+    printf("%p\n", node);
     printf("node->string:%s\n", node->string);
     printf("node->value:%s\n", node->value);
     printf("node->sibling:%p\n", node->sibling);
@@ -12,8 +13,9 @@ int main()
     printf("\n");
     printf("-----------\n");
 
-    node = radixTreeInsert(node, "helloword", "999");
+    node = radixTreeInsert(node, "add", "035");
 
+    printf("%p\n", node);
     printf("node->string:%s\n", node->string);
     printf("node->value:%s\n", node->value);
     printf("node->sibling:%p\n", node->sibling);
@@ -29,81 +31,8 @@ int main()
     printf("node->child:%p\n", n->child);
 
     printf("\n");
-    printf("-----------\n");
 
-    node = radixTreeInsert(node, "hellowordyou", "333");
-    printf("node->string:%s\n", node->string);
-    printf("node->value:%s\n", node->value);
-    printf("node->sibling:%p\n", node->sibling);
-    printf("node->child:%p\n", node->child);
-
-    printf("\n");
-
-    n = node->child;
-    printf("%p\n", n);
-    printf("node->string:%s\n", n->string);
-    printf("node->value:%s\n", n->value);
-    printf("node->sibling:%p\n", n->sibling);
-    printf("node->child:%p\n", n->child);
-
-    printf("\n");
-
-    struct RadixTree *m = n->child;
-    printf("%p\n", m);
-    printf("node->string:%s\n", m->string);
-    printf("node->value:%s\n", m->value);
-    printf("node->sibling:%p\n", m->sibling);
-    printf("node->child:%p\n", m->child);
-
-    printf("\n");
-    printf("-----------\n");
-
-    node = radixTreeInsert(node, "helloguy", "895");
-    printf("node->string:%s\n", node->string);
-    printf("node->value:%s\n", node->value);
-    printf("node->sibling:%p\n", node->sibling);
-    printf("node->child:%p\n", node->child);
-
-    printf("\n");
-
-    n = node->child;
-    printf("%p\n", n);
-    printf("node->string:%s\n", n->string);
-    printf("node->value:%s\n", n->value);
-    printf("node->sibling:%p\n", n->sibling);
-    printf("node->child:%p\n", n->child);
-
-    printf("\n");
-
-    struct RadixTree *s = n->sibling;
-    printf("%p\n", s);
-    printf("node->string:%s\n", s->string);
-    printf("node->value:%s\n", s->value);
-    printf("node->sibling:%p\n", s->sibling);
-    printf("node->child:%p\n", s->child);
-
-    printf("\n");
-    printf("-----------\n");
-    printf("\n");
-
-    node = radixTreeInsert(node, "hel", "134");
-    printf("node->string:%s\n", node->string);
-    printf("node->value:%s\n", node->value);
-    printf("node->sibling:%p\n", node->sibling);
-    printf("node->child:%p\n", node->child);
-
-    printf("\n");
-
-    n = node->child;
-    printf("%p\n", n);
-    printf("node->string:%s\n", n->string);
-    printf("node->value:%s\n", n->value);
-    printf("node->sibling:%p\n", n->sibling);
-    printf("node->child:%p\n", n->child);
-
-    printf("\n");
-
-    m = n->child;
+    struct RadixTree *m = n->sibling;
     printf("%p\n", m);
     printf("node->string:%s\n", m->string);
     printf("node->value:%s\n", m->value);
@@ -114,7 +43,8 @@ int main()
     printf("-----------\n");
     printf("\n");
 
-    node = radixTreeInsert(node, "hel", "1345");
+    node = radixTreeInsert(node, "bag", "404");
+    printf("%p\n", node);
     printf("node->string:%s\n", node->string);
     printf("node->value:%s\n", node->value);
     printf("node->sibling:%p\n", node->sibling);
@@ -122,7 +52,16 @@ int main()
 
     printf("\n");
 
-    n = node->child;
+    struct RadixTree *r = node->sibling;
+    printf("%p\n", r);
+    printf("node->string:%s\n", r->string);
+    printf("node->value:%s\n", r->value);
+    printf("node->sibling:%p\n", r->sibling);
+    printf("node->child:%p\n", r->child);
+
+    printf("\n");
+
+    n = r->child;
     printf("%p\n", n);
     printf("node->string:%s\n", n->string);
     printf("node->value:%s\n", n->value);
@@ -131,11 +70,81 @@ int main()
 
     printf("\n");
 
-    m = n->child;
+    m = n->sibling;
     printf("%p\n", m);
     printf("node->string:%s\n", m->string);
     printf("node->value:%s\n", m->value);
     printf("node->sibling:%p\n", m->sibling);
     printf("node->child:%p\n", m->child);
+
+    printf("\n");
+    printf("-----------\n");
+    printf("\n");
+
+    node = radixTreeInsert(node, "admir", "777");
+    printf("%p\n", node);
+    printf("node->string:%s\n", node->string);
+    printf("node->value:%s\n", node->value);
+    printf("node->sibling:%p\n", node->sibling);
+    printf("node->child:%p\n", node->child);
+
+    printf("\n");
+
+    r = node->sibling;
+    printf("%p\n", r);
+    printf("node->string:%s\n", r->string);
+    printf("node->value:%s\n", r->value);
+    printf("node->sibling:%p\n", r->sibling);
+    printf("node->child:%p\n", r->child);
+
+    printf("\n");
+
+    n = r->child;
+    printf("%p\n", n);
+    printf("node->string:%s\n", n->string);
+    printf("node->value:%s\n", n->value);
+    printf("node->sibling:%p\n", n->sibling);
+    printf("node->child:%p\n", n->child);
+
+    printf("\n");
+
+    m = n->sibling;
+    printf("%p\n", m);
+    printf("node->string:%s\n", m->string);
+    printf("node->value:%s\n", m->value);
+    printf("node->sibling:%p\n", m->sibling);
+    printf("node->child:%p\n", m->child);
+
+    printf("\n");
+
+    struct RadixTree *b = m->child;
+    printf("%p\n", b);
+    printf("node->string:%s\n", b->string);
+    printf("node->value:%s\n", b->value);
+    printf("node->sibling:%p\n", b->sibling);
+    printf("node->child:%p\n", b->child);
+
+    printf("\n");
+    printf("-----------\n");
+    printf("\n");*/
+
+    //node = radixTreeInsert(node, "addition", "3634");
+
+    node = radixTreeInsert(node, "admiral", "777");
+    node = radixTreeInsert(node, "admire", "792");
+    node = radixTreeInsert(node, "admires", "046");
+    node = radixTreeInsert(node, "admiralty", "257");
+    node = radixTreeInsert(node, "add", "001");
+    node = radixTreeInsert(node, "addres", "1517");
+    node = radixTreeInsert(node, "addition", "054");
+    node = radixTreeInsert(node, "additive", "200");
+    node = radixTreeInsert(node, "hello", "000");
+    node = radixTreeInsert(node, "helloworld", "009");
+    node = radixTreeInsert(node, "ad", "002");
+
+    radixTreePrint(node, 0, 1);
+
+    char *value = radixTreeLookup(node, "admiral");
+    printf("\n%s\n", value);
     return 0;
 }
